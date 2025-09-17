dd6fc4c47ba6364791024615d2ead6f0741eaed0
platoncervakov6-dot <platoncervakov6@gmail.com>
2025-09-17 18:05:31 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { optimizeCss: true },
+  // никаких experimental: optimizeCss
+  poweredByHeader: false
 };
 
 module.exports = nextConfig;
